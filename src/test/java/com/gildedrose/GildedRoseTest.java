0f9ab19fb7d39f9a1270e59f59663468abfb7196
DT0814 <729742011@qq.com>
2020-03-02 18:53:30 +0800

    add test

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -27,4 +27,14 @@ public class GildedRoseTest {
         assertThat(app.items[0].sell_in, is(0));
     }
 
+    @Test
+    public void should_quality_and_sel_in_maintain_when_name_equal_Sulfuras_Hand_of_Ragnaros() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 1, 5) };
+        GildedRose app = new GildedRose(items);
+        app.update_quality();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertThat(app.items[0].quality, is(5));
+        assertThat(app.items[0].sell_in, is(1));
+    }
+
 }
