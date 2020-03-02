f657589fcb0186653416aaa52b974782b8c17c0f
DT0814 <729742011@qq.com>
2020-03-02 18:54:50 +0800

    add testshould_quality_and_sell_in_maintain_when_name_equal_Aged_Brie_and_quality_than_50

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -28,6 +28,16 @@ public class GildedRoseTest {
     }
 
     @Test
+    public void should_quality_and_sell_in_maintain_when_name_equal_Aged_Brie_and_quality_than_50() {
+        Item[] items = new Item[] { new Item("Aged Brie", 1, 51) };
+        GildedRose app = new GildedRose(items);
+        app.update_quality();
+        assertEquals("Aged Brie", app.items[0].name);
+        assertThat(app.items[0].quality, is(51));
+        assertThat(app.items[0].sell_in, is(0));
+    }
+
+    @Test
     public void should_quality_and_sel_in_maintain_when_name_equal_Sulfuras_Hand_of_Ragnaros() {
         Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 1, 5) };
         GildedRose app = new GildedRose(items);
